f1f87f66ab75c48d8296e5a42caadb136d85891b
malvi <W089019@myscc.ca>
2024-09-06 15:06:18 -0400

    Created starter Angular file  and completed Assignment1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Malvi-Kanadia-Learning-Angular';
+  firstname:string ='malvi';
+  lastname:string='kanadia';
+
 }
